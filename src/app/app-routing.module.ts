21ae588737afd9903375f1de8f7f4d5ad2e59df8
Rodolfo <rod_dolpho@hotmail.com>
2019-10-11 18:45:09 -0300

    added node server

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
-
+import { NgModule } from "@angular/core";
+import { Routes, RouterModule } from "@angular/router";
 
 const routes: Routes = [
-  { path: '', pathMatch: 'full', redirectTo: 'cursos' },
-  { path: 'cursos', loadChildren: './cursos/cursos.module#CursosModule'}
+  { path: "", pathMatch: "full", redirectTo: "cursos" },
+  { path: "cursos", loadChildren: "./cursos/cursos.module#CursosModule" },
+  {
+    path: "upload",
+    loadChildren: "./upload-file/upload-file.module#UploadFileModule"
+  }
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
