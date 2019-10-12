45535407e0f89a2efac97c9be1af7921954b3964
Rodolfo <rod_dolpho@hotmail.com>
2019-10-12 15:21:16 -0300

    added progress bar

--- src/app/upload-file/upload-file/upload-file.component.ts
+++ src/app/upload-file/upload-file/upload-file.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 import { UploadFileService } from '../upload-file.service';
 import { environment } from '../../../environments/environment';
+import { HttpEvent, HttpEventType } from '@angular/common/http';
 
 @Component({
   selector: "app-upload-file",
@@ -12,6 +13,7 @@ export class UploadFileComponent implements OnInit {
   constructor(private uploadService: UploadFileService) { }
 
   files: Set<File>;
+  progress: number = 0;
 
   ngOnInit() { }
 
@@ -29,13 +31,22 @@ export class UploadFileComponent implements OnInit {
       this.files.add(selectedFiles[i])
     }
     document.getElementById("customFileLabel").innerHTML = fileNames.join(", ");
+    this.progress = 0;
   }
 
   onUpload() {
     if (this.files && this.files.size > 0) {
       this.uploadService.upload(this.files, environment.BASE_URL + '/api/upload')
-        .subscribe(response => {
-          console.log('Upload concluído')
+        .subscribe((event: HttpEvent<Object>) => {
+
+          console.log(event)
+          if (event.type == HttpEventType.Response) {
+            console.log('Upload Concluido ')
+          }
+          else if (event.type == HttpEventType.UploadProgress) {
+            const progressDone = Math.round((event.loaded * 100) / event.total);
+            this.progress = progressDone;
+          }
         })
     }
   }
